f2b03feb58e6290e96b54205cb9f3055ea406392
DasHeinzchen <marlon.heinz@outlook.de>
2022-02-17 22:28:21 +0100

    Split initialisation

--- structure/Split.py
+++ structure/Split.py
@@ -10,18 +10,20 @@ class Split:
         self._name = name
 
     def loadData(self):
-        if not os.path.isfile(Globals.settings["path"] + "seasons\\" + self._id + "\\" + self._id + ".json"):
-            file = open(Globals.settings["path"] + "seasons\\" + self._id + "\\" + self._id + ".json", "a")
-            file.write("{}")
-            file.close()
-        else:
-            file = open(Globals.settings["path"] + "seasons\\" + self._id + "\\" + self._id + ".json")
-            self._dict = json.load(file)
+        file = open(Globals.settings["path"] + "seasons\\" + self._id.split("_")[0] + "\\" + self._id.split("_")[1] + "\\" + self._id.split("_")[1] + ".json")
+        self._dict = json.load(file)
 
-            file.close()
+        self._id = self._dict["id"]
+        self._current = self._dict["current"]
+        self._name = self._dict["name"]
+
+        file.close()
 
     def saveData(self):
-        print()
+        self._dict.update({"id": self._id, "current": self._current, "name": self._name})
+        file = open(Globals.settings["path"] + "seasons\\" + self._id.split("_")[0] + "\\" + self._id.split("_")[1] + "\\" + self._id.split("_")[1] + ".json", "w")
+        file.write(json.dumps(self._dict, indent=5))
+        file.close()
 
 
 def initializeSplit(id):
@@ -30,3 +32,9 @@ def initializeSplit(id):
     file = open(Globals.settings["path"] + "seasons\\" + id[0] + "\\" + id[1] + "\\" + id[1] + ".json", "a")
     file.write("{}")
     file.close()
+    Split(id=id[0] + "_" + id[1]).saveData()
+
+def getSplitById(id):
+    split = Split(id=id)
+    split.loadData()
+    return split
